bb38b2ff5dc8792c70ffc354b5e60734cf5d43a3
kstepien7 <63638676+kstepien7@users.noreply.github.com>
2020-04-15 21:49:32 +0200

    Add files via upload

--- Warehouse/src/main/java/io/github/kst/Service.java
+++ Warehouse/src/main/java/io/github/kst/Service.java
@@ -11,7 +11,7 @@ import io.github.kst.warehouse.WarehouseRepository;
 import java.util.*;
 import java.util.stream.Collectors;
 
-
+@org.springframework.stereotype.Service
 public class Service {
 
 
@@ -20,9 +20,6 @@ public class Service {
     private Distance countDistance;
     private Tour tour;
 
-    public Service() {
-        this(new CourierBaseRepository(), new WarehouseRepository(), new Distance());
-    }
 
     public Service(CourierBaseRepository courierBaseRepository,WarehouseRepository warehouseRepository, Distance countDistance) {
         this.courierBaseRepository = courierBaseRepository;
@@ -32,11 +29,45 @@ public class Service {
 
     public List<Warehouse> initiateWarehouseOrdering(List<Warehouse> warehouses, Client client) {
 
+        Map<Integer, Warehouse> mapOfWarehouses = initMapOfWarehouses(warehouses);
+        double[][] distanceMatrix = prepareDistanceMatrix(mapOfWarehouses, client);
+        int msize = mapOfWarehouses.size() + 2;
+
+        // get Tour
+        int startNode = 0;
+        Tour solver = new Tour(startNode, distanceMatrix);
+        var getTour = solver.getTour();
+        // check if dummy point is at the begging then reverse
+        if (getTour.get(1) == msize)
+            Collections.reverse(getTour);
+
+        List<Warehouse> sortedWarehouses = new LinkedList<>();
+
+        for (int i = 0; i < getTour.size(); i++) {
+            var x = getTour.get(i);
+            var y = mapOfWarehouses.get(x);
+            // check if not dummy or courier base or client
+            if (y != null)
+                sortedWarehouses.add(mapOfWarehouses.get(x));
+        }
+        return sortedWarehouses;
+    }
+
+    private Map<Integer, Warehouse> initMapOfWarehouses(List<Warehouse> warehouses) {
         //sor warehouses by id
         Collections.sort(warehouses, Comparator.comparing(Warehouse::getId));
+        Map<Integer, Warehouse> mapOfWarehouses = new HashMap<>();
+        for (int i = 0; i < warehouses.size(); i++) {
+            // column number and warehouse
+            mapOfWarehouses.put(i + 1, warehouses.get(i));
+        }
+        return mapOfWarehouses;
+    }
 
+
+    private double[][] prepareDistanceMatrix(Map<Integer, Warehouse> mapOfWarehouses, Client client) {
         // warehouses + client + courierBase
-        int msize = warehouses.size() + 2;
+        int msize = mapOfWarehouses.size() + 2;
         // create table of distances
         //+ dummy point between base and client
         double[][] distanceMatrix = new double[msize + 1][msize + 1];
@@ -55,100 +86,44 @@ public class Service {
         //because of for below
         distanceMatrix[msize - 2][msize - 2] = 0;
 
-        // distances between warehouses - base, warehouses - client
-        List<Integer> warehousesId = warehouses.stream().map(Warehouse::getId).collect(Collectors.toList());
-        var baseDistances = courierBaseRepository.findDistancesForWarehouseIds(warehousesId);
-
+        // distances between warehouses - base
+        List<Integer> warehousesId = mapOfWarehouses.values().stream().map(Warehouse::getId).collect(Collectors.toList());
+        var baseDistances = courierBaseRepository.findByWarehouseIdIn(warehousesId);
 
         // distances between warehouses
-        var warehouseDistances = warehouseRepository.findDistancesForWarehouseIds(warehousesId);
-
-        //initialize hashMap
-        Map<Integer, Warehouse> mapOfWarehouses = new HashMap<>();
-
+        var warehouseDistances = warehouseRepository.findByFromWarehouseIdInAndToWarehouseIdIn(warehousesId, warehousesId);
 
         int m = 0;
         for (int i = 0; i < msize - 2; i++) {
-            // column number and warehouse
-            mapOfWarehouses.put(i + 1, warehouses.get(i));
-
             distanceMatrix[i][i] = 0;
             // base - warehouse
             var distanceBase = baseDistances.get(i).getDistance();
             distanceMatrix[0][i + 1] = distanceBase;
             distanceMatrix[i + 1][0] = distanceBase;
+            System.out.println();
             // client - warehouse
-            var distanceClient = countDistance.calculateDistanceBetweenPoints(warehouses.get(i).getLatitude(),
-                    warehouses.get(i).getLongitude(),
-                    client.getLatitude(),
-                    client.getLongitude());
-            distanceMatrix[i + 1][msize - 1] = distanceClient;
-            distanceMatrix[msize - 1][i+1] = distanceClient;
-
+            calculateDistanceBetweenClientAndWarehouse(mapOfWarehouses.get(i + 1), client, distanceMatrix, i, msize);
+            // warehouse - warehouse
             for(int j = i; j < msize - 3; j++){
                 var x = warehouseDistances.get(m).getFromWarehouseId();
                 var y = warehouseDistances.get(m).getToWarehouseId();
                 var distanceWarehouse = warehouseDistances.get(m).getDistance();
-                System.out.println("x " + x + " y " + y + " distance " + distanceWarehouse + " i " + i + " j " + j + " m " + m);
                 distanceMatrix[i+1][j+2] = distanceWarehouse;
                 distanceMatrix[j+2][i+1] = distanceWarehouse;
                 m++;
             }
         }
+        return distanceMatrix;
+    }
 
-
-
-
-
-        for (Map.Entry<Integer, Warehouse> entry : mapOfWarehouses.entrySet()) {
-            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-        }
-        for (int i = 0; i < msize + 1; i++){
-            for(int j = 0; j < msize + 1; j++){
-                System.out.println(distanceMatrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-
-
-
-        // get Tour
-        int startNode = 0;
-        Tour solver = new Tour(startNode, distanceMatrix);
-        var getTour = solver.getTour();
-        // check if dummy point is at the begging then reverse
-        if (getTour.get(1) == msize)
-            Collections.reverse(getTour);
-
-
-        List<Warehouse> sortedWarehouses = new LinkedList<>();
-
-        for (int i = 0; i < getTour.size(); i++) {
-            var x = getTour.get(i);
-            System.out.println("x = " + x);
-            var y = mapOfWarehouses.get(x);
-            System.out.println(y);
-            // check if not dummy or courier base or client
-            if (y != null)
-                sortedWarehouses.add(mapOfWarehouses.get(x));
-        }
-
-
-
-
-
-        System.out.println("Tour: " + getTour);
-        System.out.println("Tour cost: " + solver.getTourCost());
-        for(int i = 0; i < sortedWarehouses.size(); i++){
-            System.out.println("po sortach: " + sortedWarehouses.get(i));
-        }
-
-
-
-
-
-        return sortedWarehouses;
-
+    private void calculateDistanceBetweenClientAndWarehouse(Warehouse warehouse, Client client, double[][] distanceMatrix, int i, int msize) {
+        // client - warehouse
+        var distanceClient = countDistance.calculateDistanceBetweenPoints(warehouse.getLatitude(),
+                warehouse.getLongitude(),
+                client.getLatitude(),
+                client.getLongitude());
+        distanceMatrix[i + 1][msize - 1] = distanceClient;
+        distanceMatrix[msize - 1][i+1] = distanceClient;
     }
 }
+
--- Warehouse/src/main/java/io/github/kst/courierbase/CourierBaseRepository.java
+++ Warehouse/src/main/java/io/github/kst/courierbase/CourierBaseRepository.java
@@ -1,24 +1,11 @@
 package io.github.kst.courierbase;
 
-import io.github.kst.HibernateUtil;
-import org.hibernate.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public class CourierBaseRepository {
-
-    public List<DistanceToBase> findDistancesForWarehouseIds(List<Integer> warehousesId) {
-
-        var session= HibernateUtil.getSessionFactory().openSession();
-        var transaction = session.beginTransaction();
-
-        Query query = session.createQuery("from DistanceToBase DTB where DTB.warehouseId in (:warehousesId)");
-        query.setParameter("warehousesId", warehousesId);
-        List<DistanceToBase> result = (List<DistanceToBase>) ((Query) query).getResultList();
-
-        transaction.commit();
-        session.close();
-        return result;
-    }
+@Repository
+public interface CourierBaseRepository extends JpaRepository<DistanceToBase, Integer> {
+    List<DistanceToBase> findByWarehouseIdIn(List<Integer> warehouseId);
 }
-
